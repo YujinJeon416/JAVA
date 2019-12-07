ba72b7a18b293d1b1fa8c4edd0dcabf6d8e4f397
minkj1992 <minkj1992@gmail.com>
2019-12-07 16:40:05 +0900

    ch07 fin

--- ThinkDataStructures/code/src/Cat.java
+++ ThinkDataStructures/code/src/Cat.java
@@ -1,1 +1,33 @@
+public class Cat extends Animal {
+    int type;
+
+    public Cat(String name, int type) {
+        super(name);
+        this.type = type;
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    public int getType(){
+        return type;
+    }
+
+    public static void main(String[] args) {
+        String name = "minwook";
+        Cat cat = new Cat(name,1);
+        Animal animal = cat;
+
+        String curAnimalName = animal.getName();
+        String curCatName = cat.getName();
+
+//        int curAnimalType = animal.getType(); // animal 타입으로 캐스팅된 cat은 cat method를 사용할 수 없다.
+        int curCatType = cat.getType();
+        System.out.println(animal == cat);  // 그런데 어쨰서 animal == cat (true)가 될까?
+
+
+    }
+}
 
