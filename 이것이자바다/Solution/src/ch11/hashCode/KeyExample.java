c6cdb7aebeefd1189c580693e30c3a40f05ef2f3
minkj1992 <minkj1992@gmail.com>
2019-12-12 19:50:51 +0900

    ch15 JCF 완료

--- 이것이자바다/Solution/src/ch11/hashCode/KeyExample.java
+++ 이것이자바다/Solution/src/ch11/hashCode/KeyExample.java
@@ -1,22 +1,22 @@
-package ch11.hashCode;
-
-import java.util.HashMap;
-
-public class KeyExample {
-    public static void main(String[] args) {
-        HashMap<Key, String> hashMap = new HashMap<>();
-        Key k1 = new Key(1);
-        Key k2 = new Key(2);
-
-        hashMap.put(k1, "Á¦¹Î¿í");
-        hashMap.put(k2, "Á¦¹ÎÈ£");
-
-        String value = hashMap.get(new Key(1));
-        String value2 = hashMap.get(k1);
-        String value3 = hashMap.get(k2);
-
-        System.out.println(value);
-        System.out.println(value2);
-        System.out.println(value3);
-    }
-}
+//package ch11.hashCode;
+//
+//import java.util.HashMap;
+//
+//public class KeyExample {
+//    public static void main(String[] args) {
+//        HashMap<Key, String> hashMap = new HashMap<>();
+//        Key k1 = new Key(1);
+//        Key k2 = new Key(2);
+//
+//        hashMap.put(k1, "Á¦¹Î¿í");
+//        hashMap.put(k2, "Á¦¹ÎÈ£");
+//
+//        String value = hashMap.get(new Key(1));
+//        String value2 = hashMap.get(k1);
+//        String value3 = hashMap.get(k2);
+//
+//        System.out.println(value);
+//        System.out.println(value2);
+//        System.out.println(value3);
+//    }
+//}
