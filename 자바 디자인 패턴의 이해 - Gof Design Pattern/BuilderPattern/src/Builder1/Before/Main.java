855a977ba56a032fab6763e811478b7f11db90c5
minkj1992 <minkj1992@gmail.com>
2019-12-18 21:15:56 +0900

    BuilderPattern init

---
+++ 자바 디자인 패턴의 이해 - Gof Design Pattern/BuilderPattern/src/Builder1/Before/Main.java
@@ -0,0 +1,8 @@
+package Builder1.Before;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer LgGram = new Computer("i7", "16gb", "256gb SSD");
+        System.out.println(LgGram.toString());
+    }
+}
